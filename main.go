d6dae8f8c13cf8b3e7b7190a8076df70d0ba4fae
unemekenta <unemekenta@gmail.com>
2022-11-05 19:46:16 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// ルーターのイニシャライズ
+	r := mux.NewRouter()
+
+	// ルート(エンドポイント)
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
